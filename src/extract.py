c9961ec7621148a20016f44d94c9b54d0b83b6c1
Estrella Trabanca Pineda <estrellatrabancapineda@gmail.com>
2026-07-12 12:51:45 +0200

    feat: add robust data extraction module for Yahoo and News API

---
+++ src/extract.py
@@ -0,0 +1,70 @@
+"""
+Data Extraction Module for the LLMOps Finance ETL pipeline.
+This script is responsible for ingesting quantitative market data 
+and qualitative news data from external APIs.
+"""
+
+import yfinance as yf
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+NEWS_API = os.getenv("NEWS_API_KEY")
+
+
+def get_data(ticker):
+    """
+    Extracts historical stock market data for a given company.
+
+    Args:
+        ticker (str): The stock symbol of the company (e.g., 'AAPL', 'MSFT').
+
+    Returns:
+        pandas.DataFrame: A dataframe containing the 'Date', 'Close' price, 
+        and 'Volume' of the stock over the last month.
+    """
+    try: 
+        bussiness = yf.Ticker(ticker)
+        data = bussiness.history(period = "1mo")
+        filt_data = data[["Close", "Volume"]].reset_index()
+        return filt_data
+    
+    except Exception as e:
+        print(f"Error extracting financial data for {ticker}: {e}")
+        return pd.DataFrame(columns = ["Date", "Close", "Volume"])
+
+
+def get_news(topic):
+    """
+    Fetches recent news articles related to a specific topic using the News API.
+
+    Args:
+        topic (str): The keyword or search query for the news (e.g., 'Apple stock').
+
+    Returns:
+        dict: A JSON response parsed into a Python dictionary containing 
+        the retrieved news articles and metadata.
+    """
+    try: 
+        url = f"https://newsapi.org/v2/everything?q={topic}&apiKey={NEWS_API}"
+        output = requests.get(url)
+        return output.json()
+    
+    except Exception as e:
+        print(f"Error extracting news: {e}")
+        return {"status" : "error", "totalResults": 0, "articles": []}
+
+
+if __name__ == "__main__":
+    print("Extracting Data...")
+    
+    stock_data = get_data("AAPL")
+    print("\n--- Stock Data ---")
+    print(stock_data.head())
+    
+
+    news = get_news("Apple stock")
+    print("\n--- JSON News ---")
+    print(news)
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import extract
+
+print("Pipeline is initializing from main.py...")
+
+final_data = extract.get_data("MSFT")
+print(final_data)
