a6e11e1809d3049ab2a5c9df28fccb5f958c2e06
Estrella Trabanca Pineda <estrellatrabancapineda@gmail.com>
2026-07-18 17:04:20 +0200

    df added to download_data_from _bq in features file

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,37 +3,41 @@ import pandas as pd
 import numpy as np
 
 # Data exctraction
-def dowland_data():
+def download_data_from_bq(df):
     pass
 
 
 # Technical Variables
-def daily_return():
+def daily_return(df):
     pass
 
 
-def volatility():
+def volatility(df):
     pass
 
 
-def RSI():
+def RSI(df):
     pass
 
 
-def log_change_vol():
+def log_change_vol(df):
     pass
 
 
 # Qualitative Variables
-def sentiment_momentum():
+def sentiment_momentum(df):
     pass
 
 
 # Temporal varibles
-def trend():
+def cyclic_calendar_features(df):
     pass
 
 
-# 
+def calculate_target(df):
+    pass
+
+
+# Tensor transformation
 def two_weeks_span(batch_size, sequence_length, num_features):
     pass
