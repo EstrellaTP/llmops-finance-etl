c09c653bd5cf3e80610cde3c4c37acb2751986d0
Estrella Trabanca Pineda <estrellatrabancapineda@gmail.com>
2026-07-18 16:54:16 +0200

    chore: create Phase 4 file skeleton

---
+++ src/features/build_features.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import numpy as np
+
+# Data exctraction
+def dowland_data():
+    pass
+
+
+# Technical Variables
+def daily_return():
+    pass
+
+
+def volatility():
+    pass
+
+
+def RSI():
+    pass
+
+
+def log_change_vol():
+    pass
+
+
+# Qualitative Variables
+def sentiment_momentum():
+    pass
+
+
+# Temporal varibles
+def trend():
+    pass
+
+
+# 
+def two_weeks_span(batch_size, sequence_length, num_features):
+    pass
